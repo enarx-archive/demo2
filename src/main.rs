48f2a19e6f939ac57c55f2b54cbe591376946cd0
Nathaniel McCallum <npmccallum@redhat.com>
2020-09-15 08:42:59 -0400

    Fix message

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let number = u32::from_le_bytes(buf);
     let string = CString::new(format!("=={:08x}==", number)).unwrap();
 
-    println!("Press any key to reveal the secret...");
+    println!("Press ENTER to reveal the secret...");
     std::io::stdin().read_exact(&mut [0u8]).unwrap();
 
     println!("The secret is: {:?}", string);
