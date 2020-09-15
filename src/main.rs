cd30f800b02f45830dd1cb3eb7a6421becf68ee6
Nathaniel McCallum <npmccallum@redhat.com>
2020-09-15 08:42:34 -0400

    Use CString for the secret

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
+use std::ffi::CString;
 use std::io::Read;
 
 fn main() {
     let mut buf = [0u8; 4];
     let _ = getrandom::getrandom(&mut buf).unwrap();
     let number = u32::from_le_bytes(buf);
-    let string = format!("{:08x}", number);
+    let string = CString::new(format!("=={:08x}==", number)).unwrap();
 
     println!("Press any key to reveal the secret...");
     std::io::stdin().read_exact(&mut [0u8]).unwrap();
 
-    println!("The secret is: {}", string);
+    println!("The secret is: {:?}", string);
 }
